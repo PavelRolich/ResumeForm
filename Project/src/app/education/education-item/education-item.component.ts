bf0dcb1248a5cdb464335b1e65f7e68880d93d4e
Pavel <pahan.rolich@mail.ru>
2019-11-11 11:19:39 +0300

    add education function

--- Project/src/app/education/education-item/education-item.component.ts
+++ Project/src/app/education/education-item/education-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Education } from '../education.component';
 
 @Component({
   selector: 'app-education-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education-item.component.scss']
 })
 export class EducationItemComponent implements OnInit {
+  @Input() education: Education;
 
   constructor() { }
 
--- Project/src/app/education/education.component.ts
+++ Project/src/app/education/education.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  educationList = new Set<Education>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addItem(periodStart: string, periodEnd: string, studyPlace: string, faculty: string, specialty: string, qualification: string) {
+    const item: Education = {
+      periodOfTime: {
+        start: new Date(periodStart),
+        end: new Date(periodEnd),
+      },
+      studyPlace,
+      faculty,
+      specialty,
+      qualification,
+    };
+    this.educationList.add(item);
+  }
+
+}
+
+export interface Education {
+  periodOfTime: {
+    start: Date;
+    end: Date;
+  };
+  studyPlace: string;
+  faculty?: string;
+  specialty?: string;
+  qualification: string;
 }
